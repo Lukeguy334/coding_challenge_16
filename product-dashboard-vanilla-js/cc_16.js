f92212c28b0d56733aec5078a79a8df13a1a98f9
Lukeguy334 <lhs57@usf.edu>
2025-03-26 14:23:50 -0400

    Task 3 - Add fetchProductsAsync using async/await and try/catch

--- product-dashboard-vanilla-js/cc_16.js
+++ product-dashboard-vanilla-js/cc_16.js
@@ -15,4 +15,18 @@ function fetchProductsThen() {
   // Trigger the fetch function to test
   fetchProductsThen();
   
+  // Task 3: Fetch Products with async/await
+
+async function fetchProductsAsync() {
+    try {
+      const response = await fetch("https://www.course-api.com/javascript-store-products");
+      const products = await response.json();
+      displayProducts(products); // Display products on success
+    } catch (error) {
+      handleError(error); // Handle errors using helper function
+    }
+  }
+  
+  // Trigger the fetch function to test
+  fetchProductsAsync();
   
